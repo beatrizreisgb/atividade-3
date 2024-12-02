d0aea064bb6aad81981fe45e245b99dfe8f4fbbe
beatrizreisgb <beatriz.barbosa@dcc.ufmg.br>
2024-12-02 19:52:47 -0300

    melhoria da cobertura de testes -  parte 3 tp2es

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,26 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+});
+
+test('Testando cadastro de três respostas', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('3 * 27 = ?');
+  const id_resposta = modelo.cadastrar_resposta(id_pergunta, '81');
+  const respostas = modelo.get_respostas(id_pergunta);
+  const num_respostas = modelo.get_num_respostas(id_pergunta);
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('81');
+  expect(num_respostas).toBe(1);
+});
+
+test('Testando get_pergunta', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('9 * 9 = ?');
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.texto).toBe('9 * 9 = ?');
 });
